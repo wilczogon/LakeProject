714cb3efdde634502af384ba96b3ba25d280e296
Ahvaltik <pawele@vp.pl>
2014-12-28 15:45:20 +0100

    Added LakeObjectFocusListener.java

---
+++ src/com/edu/agh/student/lakeproject/userinterface/LakeObjectFocusListener.java
@@ -0,0 +1,7 @@
+package com.edu.agh.student.lakeproject.userinterface;
+
+import com.edu.agh.student.lakeproject.lakeworld.LakeObject;
+
+public interface LakeObjectFocusListener {
+	void setChosenLakeObject(LakeObject chosen);
+}
