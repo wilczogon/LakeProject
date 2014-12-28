fd6248241668b791d5addda0efbca6766a00d56c
Sławomir Pacyno <mostechmarine@lenovoX61.(none)>
2014-12-28 19:14:11 +0100

    Report generator added.

---
+++ src/com/edu/agh/student/lakeproject/lakeworld/ReportManager.java
@@ -0,0 +1,22 @@
+package com.edu.agh.student.lakeproject.lakeworld;
+
+public class ReportManager{
+
+  public ReportManager(LakeWorld lakeWorld){
+  }
+
+  public void start(){
+  }
+  
+  public void stop(){
+  }
+  
+  public void reset(){
+  }
+  
+  public void generateReport(String directoryName){
+  }
+  
+  public void step(){
+  }
+} 
