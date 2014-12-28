03ed5d42208cb51d19977fdadb86759543231416
Sławomir Pacyno <mostechmarine@lenovoX61.(none)>
2014-12-28 20:10:51 +0100

    another some changes to Report Manager

--- src/com/edu/agh/student/lakeproject/lakeworld/ReportManager.java
+++ src/com/edu/agh/student/lakeproject/lakeworld/ReportManager.java
@@ -40,18 +40,26 @@ public class ReportManager{
     return isStopped;
   }
   
-  private void count(){ //TODO
+  private void count(){
     List<LakeObject> lakeObjects = lakeWorld.getLakeObjects();
     
     for(LakeObject lakeObject: lakeObjects){
       if(lakeObject instanceOf Fish){
 	Fish fish = (Fish)lakeObject;
 	String spaceName = fish.getSpace();
+	boolean reportDataFound = false;
 	
 	for(ReportData reportData: report){
 	  if(spaceName.equals(reportData.getSpaceName())){
 	    reportData.reportFishLifeTime(fish.getAge());
+	    reportDataFound = true;
+	    break;
 	  }
+	}
+	
+	if(!reportedDataFound){
+	  ReportData reportData = new ReportData(fish.getSpace());
+	  reportData.reportFishLifeTime(fish.getAge());
 	}
       }
     }
